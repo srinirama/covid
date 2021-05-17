8eb2a18516ec7007f0d389237d8504bcba4c03f5
srinirama <36499607+srinirama@users.noreply.github.com>
2021-05-17 12:01:12 +0530

    Utility to Find the Slot Availability based on Pincode and Date

---
+++ covid.ps1
@@ -0,0 +1,28 @@
+﻿param ($pincode, $date, $HospitalName)
+
+if (!$pincode -and !$date  ) { Write-Host "Usage : powershell .\covid.ps1 -pincode 600100 -date 14-05-2021 " 
+break;
+ }
+
+$response = Invoke-WebRequest -Uri "https://cdn-api.co-vin.in/api/v2/appointment/sessions/calendarByPin?pincode=$pincode&date=$date" -Method Get -UseBasicParsing -ErrorAction SilentlyContinue
+
+
+if (!$response.content ) { Write-Host "No Slots available" 
+break;
+ }
+
+$output = $response.content | ConvertFrom-Json
+
+$session = $output | select -expand centers 
+$session.sessions
+$session | ForEach-Object {
+$hospital = $_.name
+
+$_.sessions.ForEach({ $result = $_ | Format-Table -Property @{L='Hospital';E={$hospital}},@{L='Age';E={$_.min_age_limit}},@{L='Vaccine';E={$_.vaccine}},@{L='Availability';E={$_.available_capacity}},@{L='Date';E={$_.date}}|  Out-String  
+
+$wshell = New-Object -ComObject Wscript.Shell 
+$Output = $wshell.Popup($result)
+
+}) 
+}
+
